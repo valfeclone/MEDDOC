3466e511df9c94925be7b7aec27ec0eb432d0e08
valfeclone <fadhlanpasyah13@gmail.com>
2020-11-09 21:48:58 +0700

    ketinggalan benerin model dikit

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,11 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\Admin as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Sanctum\HasApiTokens;
 
 class Admin extends Authenticatable
